719881c4c48fb80ccb6fe695149d2e930cc22b20
fatmamutlu2514 <118067905+fatmamutlu2514@users.noreply.github.com>
2022-12-31 20:39:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import tas from './img/tas.jpg'
+import makas from './img/makas.jpg'
+import kagit from './img/kagit.jpg'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [kullaniciSecim, setkullaniciSecim] = useState(null)
+  const [kullaniciResim, setkullaniciResim] = useState(null)
+  const [bilgisayarResim, setbilgisayarResim] = useState(null)
+  const [kazanan, Setkazanan] = useState(null)
+  const [BilgisayarSecimi, setBilgisayarSecimi] = useState(null)
+
+
+  const secimler = ['tas', 'kagit', 'makas']
+
+  const kazanmaSarti = {
+    makas: ["tas"],
+    tas: ["kagit"],
+    kagit: ["makas"]
+  };
+
+  const handleClick = (e) => {
+    if (e === 'tas') {
+      setkullaniciResim(tas)
+    }
+    else if (e === 'makas') {
+      setkullaniciResim(makas)
+    }
+    else if (e === 'kagit') {
+      setkullaniciResim(kagit)
+    }
+    setkullaniciSecim(e)
+    bilgisayarSecimi();
+  }
+  const bilgisayarSecimi = () => {
+    const keys = Object.keys(kazanmaSarti)
+    const index = Math.floor(Math.random() * keys.length)
+    console.log(keys[index])
+
+    if (keys[index] === 'tas') {
+      setbilgisayarResim(tas)
+    }
+    else if (keys[index] === 'makas') {
+      setbilgisayarResim(makas)
+    }
+    else if (keys[index] === 'kagit') {
+      setbilgisayarResim(kagit)
+    }
+    setBilgisayarSecimi(keys[index])
+
+  }
+
+  function kazananiBelirle(kullaniciSecim, bilgisayarSecim) {
+    if (kullaniciSecim === bilgisayarSecim) {
+      return "Berabere!";
+    }
+    else if (kazanmaSarti[kullaniciSecim].includes(bilgisayarSecim)) {
+      return "Bilgisayar kazandı!";
+    }
+    else if (kazanmaSarti[bilgisayarSecim].includes(kullaniciSecim)) {
+      return "Sen kazandın!";
+    }
+  }
+
+  useEffect(() => {
+    {
+      if (kullaniciSecim != null) {
+        Setkazanan(kazananiBelirle(kullaniciSecim, BilgisayarSecimi))
+      }
+
+
+    }
+  }, [BilgisayarSecimi, kullaniciSecim])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Denemeyeninn
-        </a>
-      </header>
+    <div className='output'>
+      <h2>kullanıcının seçimi: {kullaniciSecim}</h2>
+      <img src={kullaniciResim}></img>
+      <h2>bilgisayarın seçimi: {BilgisayarSecimi}</h2>
+      <img src={bilgisayarResim}></img>
+      {secimler.map((secim, index) =>
+        <button key={index} onClick={() => handleClick(secim)}>
+          {secim}
+        </button>
+      )}
+      <h2>{kazanan}</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
